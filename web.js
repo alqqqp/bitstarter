ecaaee31db6795a826eba44d6b0c7f57ddcc38f0
alqqqp <ignis_i@web.de>
2013-07-10 09:11:43 +0000

    modified web.js again

--- web.js
+++ web.js
@@ -1,12 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
-var buffer_contents = fs.readFileSync('index.html', 'utf-8');
-var string = buffer_contents.toString();
+var buffer_contents = fs.readFileSync('index.html','utf-8');
+var string_contents = buffer_contents.toString();
 
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
-    response.send(string);
+    response.send(string_contents);
 });
 
 var port = process.env.PORT || 5000;
