74f289786e03efef31295fdc7b9f886026405555
alqqqp <ignis_i@web.de>
2013-08-05 14:36:42 +0000

    Edited product desc in index.html

--- web.js
+++ web.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
-var buffer_contents = fs.readFileSync('index.html','utf-8');
-var string_contents = buffer_contents.toString();
 
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
-    response.send(string_contents);
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
